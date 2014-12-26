6ceaa87a8767f3348a3b1fe125b55133ca48d7e6
fangjian <fang.jian@126.com>
2014-12-26 16:45:21 +0800

    add User.java

---
+++ src/User.java
@@ -0,0 +1,11 @@
+/**
+ * Created by fangjian on 2014/12/26.
+ */
+public class User {
+    private String name;
+    private String age;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
